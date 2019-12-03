faf1b92984750c6ca95b186423676ca5d675dd03
jester91 <50679897+jester91@users.noreply.github.com>
2019-12-03 17:31:53 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,21 @@
+(function(){
+
+    var preload=document.getElementById("preload");
+    var loading=0;
+    var id=setInterval(frame,64);
+
+    function frame(){
+        if(loading==40){
+            clearInterval(id);
+            window.open("welcome.html","_self");
+        }
+        else{
+            loading=loading+1;
+            if(loading===30){
+                preload.style.animation="fadeout 1s ease";
+            }
+        }
+    }
+
+
+})();
